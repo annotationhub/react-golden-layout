d2608fe1c664d0e3082fe7d5cb9f470dd93673f7
lsimkins <luke@bitparadigm.com>
2020-06-30 18:44:13 -0500

    GoldenLayoutComponent will now handle component registering internally.
    1. Removes need for the registerComponent call. Now happens under the hood.
    2. Removes arbitrary component string from config. Now you can simply pass in a react component instead.
    3. Removes the need to specify `type: “react-component”` in config.

--- src/js_es6/utils/ReactLayoutComponent.js
+++ src/js_es6/utils/ReactLayoutComponent.js
@@ -2,9 +2,87 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LayoutManager from '../LayoutManager';
 
+/**
+ * Far from cryptographically secure, but good enough to avoid component naming collisions.
+ */
+function randomString() {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
+
+function translateConfig(config, configMap) {
+  if (config.component) {
+    const componentName = `${(config.component.key || config.component.name)}_${randomString()}`;
+    configMap[componentName] = config.component;
+
+    return {
+      ...config,
+      type: 'react-component',
+      component: componentName,
+    };
+  }
+
+  return {
+    ...config,
+    content: config.content.map(item => translateConfig(item, configMap))
+  };
+}
+
 export default class ReactLayoutComponent extends React.Component {
-  state = {};
   containerRef = React.createRef();
+
+  constructor(props) {
+    super(props);
+
+    const componentMap = {};
+    const glConfig = translateConfig(this.props.config || {}, componentMap);
+
+    this.state = {
+      renderPanels: new Set(),
+      config: this.props.config,
+      glConfig,
+      componentMap
+    };
+  }
+
+  /**
+   * Called by ReactComponentHandler on GoldenLayout's render component call.
+   * @param {ReactComponentHandler} reactComponentHandler 
+   */
+  componentRender(reactComponentHandler) {
+    this.setState(state => {
+      let newRenderPanels = new Set(state.renderPanels);
+      newRenderPanels.add(reactComponentHandler);
+      return { renderPanels: newRenderPanels };
+    });
+  }
+
+  /**
+   * Called by ReactComponentHandler on GoldenLayout's destroy component call.
+   * @param {ReactComponentHandler} reactComponentHandler 
+   */
+  componentDestroy(reactComponentHandler) {
+    this.setState(state => {
+      let newRenderPanels = new Set(state.renderPanels);
+      newRenderPanels.delete(reactComponentHandler);
+      return { renderPanels: newRenderPanels };
+    });
+  }
+
+  goldenLayoutInstance = undefined;
+
+  componentDidMount() {
+    this.goldenLayoutInstance = new LayoutManager(
+      this.state.glConfig || {},
+      this.containerRef.current
+    );
+
+    for (let component in this.state.componentMap) {
+      this.goldenLayoutInstance.registerComponent(component, this.state.componentMap[component]);
+    }
+
+    this.goldenLayoutInstance.reactContainer = this;
+    this.goldenLayoutInstance.init();
+  }
 
   render() {
     let panels = Array.from(this.state.renderPanels || []);
@@ -19,37 +97,5 @@ export default class ReactLayoutComponent extends React.Component {
         })}
       </div>
     );
-  }
-
-  componentRender(reactComponentHandler) {
-    this.setState(state => {
-      let newRenderPanels = new Set(state.renderPanels);
-      newRenderPanels.add(reactComponentHandler);
-      return { renderPanels: newRenderPanels };
-    });
-  }
-
-  componentDestroy(reactComponentHandler) {
-    this.setState(state => {
-      let newRenderPanels = new Set(state.renderPanels);
-      newRenderPanels.delete(reactComponentHandler);
-      return { renderPanels: newRenderPanels };
-    });
-  }
-
-  goldenLayoutInstance = undefined;
-
-  componentDidMount() {
-    this.goldenLayoutInstance = new LayoutManager(
-      this.props.config || {},
-      this.containerRef.current
-    );
-
-    if (this.props.registerComponents instanceof Function) {
-      this.props.registerComponents(this.goldenLayoutInstance);
-    }
-
-    this.goldenLayoutInstance.reactContainer = this;
-    this.goldenLayoutInstance.init();
   }
 }
--- webpack/demo/webpack.config.js
+++ webpack/demo/webpack.config.js
@@ -34,7 +34,14 @@ module.exports = {
                 use: {
                     loader: "babel-loader",
                     options: {
-                        presets: ["@babel/preset-env"],
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                        ],
+                        plugins: [
+                            "@babel/plugin-proposal-class-properties",
+                            "transform-react-jsx"
+                        ]
                     },
                 },
             },
